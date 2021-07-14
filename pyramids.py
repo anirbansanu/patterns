151931225f133e672f6447de510150a9d4ad96ca
Anirban <anirbanmukherjee602@gmail.com>
2021-07-14 21:39:56 +0530

    comments added

--- pyramids.py
+++ pyramids.py
@@ -1,4 +1,4 @@
-
+# print pyramid pattern using loops
 def pyramid(n):
     for i in range(1, n + 1):
         for j in range(0, n - i):
@@ -16,6 +16,7 @@ def pyramidUsingOneLoop(n):
         print("  " * j + "* " * k)
 
 
+# print reverse pyramid pattern using loops
 def revPyramid(n):
     for i in range(n, 0, -1):
         for j in range(0, n - i):
