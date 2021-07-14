fecc065fff81e55d774a3bbe604d8dac66547a86
Anirban <anirbanmukherjee602@gmail.com>
2021-07-14 23:43:52 +0530

    Square Border Using Only One Loop Added

--- square_patterns.py
+++ square_patterns.py
@@ -5,5 +5,30 @@ def square(n):
         print()
 
 
+def squareBorder(n):
+    for rows in range(0, n):
+        for cols in range(0, n):
+            if cols == 0 or cols == n-1 or rows == 0 or rows == n-1:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+
+def squareBorderUsingOnlyOneLoop(n):
+    for rows in range(0, n):
+        print("*", end=" ")
+        if rows == 0 or rows == n-1:
+            print("* "*(n-2), end="")
+        else:
+            print("  "*(n-2), end="")
+        print("* ")
+
+
 if __name__ == "__main__":
-    square(4)
+    square(5)
+    print()
+    squareBorder(5)
+    print()
+    print()
+    squareBorderUsingOnlyOneLoop(5)
